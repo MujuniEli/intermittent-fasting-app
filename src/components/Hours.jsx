7c6d342f9f4b559d2045d4af39257a504cdcc2c6
MujuniEli <elijahmujuni@gmail.com>
2023-08-28 08:36:57 +0300

    added more hours mark up

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -3,8 +3,15 @@
 const Hours = () => {
   return (
     <main>
-        <h3>Choose your fasting hours interval</h3>
+        <h3>Choose your fasting hours Schedule</h3>
         
+            <input type="checkbox" id="16/8Schedule" />
+            <label htmlFor="16/8Schedule">16/8 Schedule</label> <span className="info-symbol">&#8505;</span>
+            <br />
+            <input type="checkbox" id="14/10Schedule" />
+            <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
+            
+      
 
     </main>
   )
