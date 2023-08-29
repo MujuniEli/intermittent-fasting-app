43b8ef87bc6af268f44e99236ba094a988ff435e
MujuniEli <elijahmujuni@gmail.com>
2023-08-29 21:19:42 +0300

    Added slider dependency

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,6 +1,10 @@
 
 
 const Hours = () => {
+
+            const hours = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm']
+
+
   return (
     <main>
         <h3>Choose your fasting hours Schedule</h3>
@@ -10,7 +14,12 @@ const Hours = () => {
             <br />
             <input type="checkbox" id="14/10Schedule" />
             <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
-            
+
+              <div className="card">
+                {hours.map((item) => (
+                  item
+                ))}
+              </div>
       
 
     </main>
