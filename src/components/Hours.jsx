6b75a9fbe5ae2b1dc34047e75ac9ba00d8e38fd8
MujuniEli <elijahmujuni@gmail.com>
2023-09-03 15:29:15 +0300

    Style updates

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,57 +1,74 @@
-
+import { useState } from "react";
+import Slider from "react-slick";
 
 const getHours = () => {
- 
   let hours = [];
-  
+
   for (let i = 0; i < 24; i++) {
-        let hour = i % 12;
-        let suffix = i < 12 ? "AM" : "PM";
-    
-          if (hour === 0) {
-            hour = 12;
-          }
-            hours.push(hour + " " + suffix);
-            }
-        
-              return hours;
-}
+    let hour = i % 12;
+    let suffix = i < 12 ? "AM" : "PM";
 
-let displayHours = getHours();
+    if (hour === 0) {
+      hour = 12;
+    }
+    hours.push(hour + " " + suffix);
+  }
 
+  return hours;
+};
 
-
+const displayHours = getHours();
 
 const Hours = () => {
-          
-            const hourCards = displayHours.map((hour) => (
-              <div key={hour}>
-                <h4 className="solo-card">{hour}</h4>
-              </div>
-            )
-            )
+  const [sliderIndex, setSliderIndex] = useState(0);
 
-          return (
-                      <main>
-                          <h3>Choose your fasting hours Schedule</h3>
-                              <div className="schedules">
-                              <input type="checkbox" id="16/8Schedule" />
-                              <label htmlFor="16/8Schedule">16/8 Schedule</label> <span className="info-symbol">&#8505;</span>
-                              </div>
-                              
-                              <div className="schedules">
-                              <input type="checkbox" id="14/10Schedule" />
-                              <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
-                              </div>
-                              
-                              
-                              <div className="card">
-                                {hourCards}
-                                  
-                              </div>
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+  };
 
-                      </main>
-  )
-}
+  const handleSliderNext = () => {
+    setSliderIndex(sliderIndex + 1);
+  };
 
-export default Hours
+  const handleSliderPrev = () => {
+    setSliderIndex(sliderIndex - 1);
+  };
+
+  const hourCards = displayHours.map((hour) => (
+    <div key={hour}>
+      <h4 className="solo-card">{hour}</h4>
+    </div>
+  ));
+
+  return (
+    <main>
+      <h3>Choose your fasting hours Schedule</h3>
+      <div className="schedules">
+        <input type="checkbox" id="16/8Schedule" />
+        <label htmlFor="16/8Schedule">16/8 Schedule</label>{" "}
+        <span className="info-symbol">&#8505;</span>
+      </div>
+
+      <div className="schedules">
+        <input type="checkbox" id="14/10Schedule" />
+        <label htmlFor="14/10Schedule">14/10 Schedule</label>{" "}
+        <span className="info-symbol">&#8505;</span>
+      </div>
+
+      <div className="slider-container">
+      <button onClick={handleSliderPrev}>Previous</button>
+        {/* <Slider {...settings} initialSlide={sliderIndex}> */}
+          {hourCards}
+        {/* </Slider> */}
+      <button onClick={handleSliderNext}>Next</button>
+        
+      </div>
+    </main>
+  );
+};
+
+export default Hours;
