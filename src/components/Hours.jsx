4eefa070fc2e2b61fb7917102a0559c43c7330b6
MujuniEli <elijahmujuni@gmail.com>
2023-09-03 08:47:13 +0300

    Added styles

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -34,12 +34,16 @@ const Hours = () => {
           return (
                       <main>
                           <h3>Choose your fasting hours Schedule</h3>
-                          
+                              <div className="schedules">
                               <input type="checkbox" id="16/8Schedule" />
                               <label htmlFor="16/8Schedule">16/8 Schedule</label> <span className="info-symbol">&#8505;</span>
-                              <br />
+                              </div>
+                              
+                              <div className="schedules">
                               <input type="checkbox" id="14/10Schedule" />
                               <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
+                              </div>
+                              
                               
                               <div className="card">
                                 {hourCards}
