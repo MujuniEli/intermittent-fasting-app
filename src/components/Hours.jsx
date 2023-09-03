00cd0c53edbd6f104c2c1ecd4b8fc74ce193b217
MujuniEli <elijahmujuni@gmail.com>
2023-09-03 08:38:08 +0300

    Displayed hour cards

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,28 +1,52 @@
 
 
+const getHours = () => {
+ 
+  let hours = [];
+  
+  for (let i = 0; i < 24; i++) {
+        let hour = i % 12;
+        let suffix = i < 12 ? "AM" : "PM";
+    
+          if (hour === 0) {
+            hour = 12;
+          }
+            hours.push(hour + " " + suffix);
+            }
+        
+              return hours;
+}
+
+let displayHours = getHours();
+
 
 
 
 const Hours = () => {
-          const hours = Array.from({ length: 24 }, (_, i) => i + 1);
-            
-  return (
-    <main>
-        <h3>Choose your fasting hours Schedule</h3>
-        
-            <input type="checkbox" id="16/8Schedule" />
-            <label htmlFor="16/8Schedule">16/8 Schedule</label> <span className="info-symbol">&#8505;</span>
-            <br />
-            <input type="checkbox" id="14/10Schedule" />
-            <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
+          
+            const hourCards = displayHours.map((hour) => (
+              <div key={hour}>
+                <h4 className="solo-card">{hour}</h4>
+              </div>
+            )
+            )
 
-            <div className="card">
-              {hours.map((hr) => (
-                hr
-              ))}
-            </div>
+          return (
+                      <main>
+                          <h3>Choose your fasting hours Schedule</h3>
+                          
+                              <input type="checkbox" id="16/8Schedule" />
+                              <label htmlFor="16/8Schedule">16/8 Schedule</label> <span className="info-symbol">&#8505;</span>
+                              <br />
+                              <input type="checkbox" id="14/10Schedule" />
+                              <label htmlFor="14/10Schedule">14/10 Schedule</label> <span className="info-symbol">&#8505;</span>
+                              
+                              <div className="card">
+                                {hourCards}
+                                  
+                              </div>
 
-    </main>
+                      </main>
   )
 }
 
